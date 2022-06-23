4cecf4d8a90e342f64762c3c15fa33e85dfb091f
Besteven <stefanostei18@gmail.com>
2022-06-23 12:15:59 +0300

    lab4/1.cpp add

--- lab4/1.cpp
+++ lab4/1.cpp
@@ -1,28 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-void prnums(int,int *);
-main()
+
+void prnums(int, int *);
+
+int main()
 {
-	int num,*p;
+	int num, *p;
 	printf("Dwse num: ");
-	scanf("%d",&num);
-	p=(int*)calloc(sizeof(int),num);
-	prnums(num,p);
-	for(int j=0; j<num; j++)
+	scanf("%d", &num);
+	p=(int*)calloc(sizeof(int), num);
+	prnums(num, p);
+	for(int j = 0; j < num; j++)
 	{
-		printf("\nDwse ton %do arithmo: ",j+1);
-		scanf("%d",&p[j]);
+		printf("\nGive a number: ");
+		scanf("%d", &p[j]);
 	}
-	prnums(num,p);
+	prnums(num, p);
+	
+	return 0;
 }
-void prnums(int i,int *p)
+void prnums(int i, int *p)
 {
 	printf("\n");
-	for(int j=0; j<i; j++)
+	for(int j = 0; j < i; j++)
 	{
-		printf("pin[%d]: %d\n",j,p[j]);
+		printf("pin[%d]: %d\n", j, p[j]);
 	}
 	printf("\n");
-	for(int j=0; j<30; j++)
+	for(int j = 0; j < 30; j++)
 		printf("-");
 }
